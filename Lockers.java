cc52c6fb17fd5f9bf09da08c62979dc2b8ab5bb5
raghunandanj007 <raghunandanj007@ip-172-31-25-211.ec2.internal>
2022-08-06 08:14:30 +0000

    Option1 in main menu

--- Lockers.java
+++ Lockers.java
@@ -1,10 +1,15 @@
 package testPack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class Lockers {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		//Welcome Screen and Developer Details
 		System.out.println("Welcome to LockedMe.com");
@@ -23,10 +28,20 @@ public class Lockers {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("\nEnter your choice: ");
 		int option = sc.nextInt();
+		if(option!=1 && option!=2 && option!=3){
+			System.out.println("\nYou have entered an invalid choice");
+		}
+		
 		switch(option) {
 		case 1:
-			System.out.println("\nDisplaying current files");
-			break;
+			
+			//code for displaying all files in the current directory
+		    //List of all files and directories
+			  File directoryPath = new File("/home/raghunandanj007/eclipse-workspace/Test/src/testPack");
+		      ArrayList<String> dp = new ArrayList<String>(Arrays.asList(directoryPath.list()));
+		      Collections.sort(dp);
+		      System.out.println("\nList of files in ascending order is: \n"+dp);
+		      break;
 			
 		case 2:
 			System.out.println("\nChoose one of the below options");
@@ -42,6 +57,7 @@ public class Lockers {
 		}
 			
 	
+
 
 
 }
