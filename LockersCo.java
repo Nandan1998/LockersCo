9afb583e62c67a54bb5186a72dd361b5a5055d8c
raghunandanj007 <raghunandanj007@ip-172-31-25-211.ec2.internal>
2022-08-10 17:39:30 +0000

    Welcome Screen

---
+++ LockersCo.java
@@ -0,0 +1,15 @@
+package testPack;
+
+public class LockersCo {
+
+	public static void main(String[] args) {
+		// Welcome Screen and Developer Details
+		System.out.println("Welcome to LockedMe.com");
+		System.out.println("\nDeveloper Details: ");
+		System.out.println("\nName: Raghunandan J");
+		System.out.println("\nDesignation: Full Stack Developer");
+		System.out.println("\nCompany : Lockers Pvt Ltd");
+
+	}
+
+}
