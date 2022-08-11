0f7b25c4f3865e8eec92f642588ef3ccd52d3ce0
raghunandanj007 <raghunandanj007@ip-172-31-25-211.ec2.internal>
2022-08-11 18:23:51 +0000

    File handling features added

--- LockersCo.java
+++ LockersCo.java
@@ -1,6 +1,7 @@
 package testPack;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,6 +19,14 @@ public class LockersCo {
 		System.out.println("\nOption 2: Interface options");
 		System.out.println("\nOption 3: Close the application");
 	}
+		// File handling menu
+	public static void InterfaceMenu() {
+		System.out.println("\nOption 1: Add a file to the existing list");
+		System.out.println("\nOption 2: Delete a chosen file from the directory");
+		System.out.println("\nOption 3: Search a specified file in the directory");
+		System.out.println("\nOption 4: Return to the main menu"); 
+		System.out.println("\nChoose one of the above options");
+	}
 
 	public static void CurrentFiles() {
 		// code for displaying all files in the current directory
@@ -26,6 +35,20 @@ public class LockersCo {
 		ArrayList<String> dp = new ArrayList<String>(Arrays.asList(directoryPath.list()));
 		Collections.sort(dp);
 		System.out.println("\nList of files in ascending order is: \n" + dp);
+	}
+	
+	//method to add a new file to the directory
+	public static void addFile(Scanner sc) {
+		System.out.println("Please type the file name\n");
+		String fileNameInput = sc.next();
+		fileNameInput = new String(fileNameInput.concat(".txt"));
+		File file = new File(Directory + fileNameInput);
+		try {
+			file.createNewFile();
+			System.out.println("File Name:  " + file.getName() + " has been created successfully");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 
@@ -47,8 +70,16 @@ public class LockersCo {
 		int menu = MM.nextInt();
 		if (menu == 1) {
 			CurrentFiles();
-		}
-
+		} else if (menu == 2) {
+			InterfaceMenu();
+		} else if (menu == 3) {
+			System.out.println("Closing application...See you next time");
+			System.exit(0);
 	}
-
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		if (input == 1) {
+			addFile(sc);
+		}
+}
 }
